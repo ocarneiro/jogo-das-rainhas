349901c49e43c2ddce7edef5de5605d9d58c146a
Otavio Carneiro <git@carneiro.blog.br>
2019-05-27 20:54:29 -0300

    Voo

--- jogo.py
+++ jogo.py
@@ -1,12 +1,24 @@
 # coding: utf-8
 import pygame
+
 pygame.init()
 tela = pygame.display.set_mode((700,500))
-tela.fill([255,255,255])
 pygame.display.set_caption("Meu game")
 relogio = pygame.time.Clock()
-relogio.tick(60)
-img_dragao = pygame.image.load('dragao.png')
-area_dragao = pygame.Rect(30, 30, 200, 115)
-tela.blit(img_dragao, area_dragao)
-pygame.display.flip()
+
+img_dragao = pygame.image.load('imagens/dragao.png')
+pos_dragao = -200
+rodando = True
+
+while rodando:
+    relogio.tick(24)
+    tela.fill([255,255,255])
+    area_dragao = pygame.Rect(pos_dragao, 30, 200, 112)
+    tela.blit(img_dragao, area_dragao)
+    pygame.display.flip()
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+    pos_dragao = pos_dragao + 2
