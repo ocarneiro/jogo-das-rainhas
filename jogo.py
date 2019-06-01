f21edc404c2c22df183f9c102022cd1ed794d719
Otavio Carneiro <git@carneiro.blog.br>
2019-06-01 16:47:42 -0300

    Inclui a flecha

--- jogo.py
+++ jogo.py
@@ -6,12 +6,14 @@ tela = pygame.display.set_mode((700,500))
 pygame.display.set_caption("Jogo das Rainhas")
 relogio = pygame.time.Clock()
 
+# carrega imagens
 img_dragao = pygame.image.load('imagens/dragao.png')
+# reduz tamanho do dragão
 img_dragao = pygame.transform.scale(img_dragao, (110,60))
-
 img_cenario = pygame.image.load('imagens/cenario.png')
+img_seta = pygame.image.load('imagens/seta.png')
 
-pos_dragao = -200
+pos_dragao_x = -200
 vel_dragao = 6
 rodando = True
 
@@ -22,7 +24,8 @@ while rodando:
     tela.fill([255,255,255])
     # blit recebe uma imagem e uma posição (x,y)
     tela.blit(img_cenario, (0,0))
-    tela.blit(img_dragao, (pos_dragao,30))
+    tela.blit(img_seta, (344,378))
+    tela.blit(img_dragao, (pos_dragao_x,30))
     pygame.display.flip()
 
     for evento in pygame.event.get():
@@ -30,4 +33,4 @@ while rodando:
             rodando = False
 
     # movimenta o dragao em x
-    pos_dragao = pos_dragao + vel_dragao
+    pos_dragao_x = pos_dragao_x + vel_dragao
